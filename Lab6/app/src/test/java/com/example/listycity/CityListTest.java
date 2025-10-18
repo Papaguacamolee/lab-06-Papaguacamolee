e816963b9698044cb7a7497109e0c222dc224ee3
PapaGuacamolee <jmmcc52@hotmail.com>
2025-10-17 19:18:06 -0600

    final files

---
+++ Lab6/app/src/test/java/com/example/listycity/CityListTest.java
@@ -0,0 +1,23 @@
+package com.example.listycity;
+
+
+
+class CityListTest {
+    private CityList mockCityList() {
+        CityList cityList = new CityList();
+        cityList.add(mockCity());
+        return cityList;
+    }
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
+    }
+    @Test
+    void testAdd() {
+        CityList cityList = mockCityList();
+        assertEquals(1, cityList.getCities().size());
+        City city = new City("Regina", "Saskatchewan");
+        cityList.add(city);
+        assertEquals(2, cityList.getCities().size());
+        assertTrue(cityList.getCities().contains(city));
+    }
+}
